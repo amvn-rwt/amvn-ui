db59e0da9e3916a213a9a8da54f35f2c1a85e44d
Aman Rawat <amanrawat4060@gmail.com>
2026-09-13 16:17:38 +0530

    refactor(button): update button size styles for improved consistency and visual appeal

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -23,9 +23,9 @@ const buttonVariants = cva(
         success: "bg-success text-success-foreground hover:bg-success/intense",
       },
       size: {
-        sm: "h-5 px-2 text-xs",
-        md: "h-6 px-3 text-sm",
-        icon: "size-6",
+        sm: "h-6 px-2 text-xs",
+        md: "h-8 px-3 text-sm",
+        icon: "size-8",
       },
     },
     defaultVariants: {
